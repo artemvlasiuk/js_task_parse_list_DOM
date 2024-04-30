538b8e51d3f5b909ffd26ae1e09b9a8b13157554
Artem Vlasiuk <artemvlasiuk@93gmail.com>
2024-05-01 01:24:54 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,28 @@
+"use strict";
+const data = document.querySelectorAll("ul");
+const employees = [
+    ...document.querySelectorAll("li")
+];
+function convertSalary(salary) {
+    const salaryString = salary.dataset.salary;
+    return +salaryString.replace("$", "").replace(",", "");
+}
+function sortList(list) {
+    list.sort((a, b)=>convertSalary(b) - convertSalary(a)).forEach((employee)=>{
+        data.forEach((ul)=>{
+            ul.append(employee);
+        });
+    });
+}
+function getEmployees(list) {
+    return list.map((item)=>({
+            name: item.innerText,
+            position: item.dataset.position,
+            salary: item.dataset.salary,
+            age: item.dataset.age
+        }));
+}
+sortList(employees);
+getEmployees(employees);
+
+//# sourceMappingURL=index.f75de5e1.js.map
